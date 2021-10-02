ce4a8d197a7589b38bfbe49901349d970c3f7f16
Fabricio C Zuardi <fabricio@fabricio.org>
2021-10-02 15:37:55 -0300

    basic react with basic webpack config
    Boring / frustrating / infuriating stupid boilerplate
    for web apps in 2021.
    
    If you dont like "create-react-app" you live in pain.
    
    This patch contains a code formatter setup (prettier),
    dependencies for modern javascript and jsx to be transpiled (babel),
    a development web server (webpack dev server), and a webpack config
    that have the most basic render-react-inside-an-html setup.
    
    Dont be like me, use the stupid cli to create boilerplates instead.

---
+++ host-ui/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+}
