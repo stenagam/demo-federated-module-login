94725c55e564914b3e6cebe8413352a4d8ceb539
Fabricio Zuardi <fabricio.zuardi@luizalabs.com>
2021-10-04 17:18:49 -0300

    host login with keycloak-js and react-keycloak

--- host-1-ui/src/bootstrap.js
+++ host-1-ui/src/bootstrap.js
@@ -1,13 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import Component1 from "module1/Component1"
+import { ReactKeycloakProvider, useKeycloak } from "@react-keycloak/web"
+import keycloak from "./keycloak"
+
+const LoginButton = () => {
+  const { keycloak, initialized } = useKeycloak()
+  const [profile, setProfile] = useState({})
+  console.log({ keycloak, initialized })
+  if (!initialized || !keycloak || !keycloak.login) {
+    return null
+  }
+  const { authenticated, login, logout, tokenParsed } = keycloak
+  if (!authenticated) {
+    return <button onClick={login}>Login</button>
+  }
+  return (
+    <>
+      <dt>Parsed token:</dt>
+      <dd><pre>{JSON.stringify(tokenParsed, ' ', 2)}</pre></dd>
+      <button onClick={logout}>Logout</button>
+    </>
+  )
+}
 
 const App = () => (
-  <>
+  <ReactKeycloakProvider authClient={keycloak}>
     <h1>Host 1</h1>
+    <p>
+      <LoginButton />
+    </p>
     <div style={{ border: "1px solid gray" }}>
       <Component1 />
     </div>
-  </>
+  </ReactKeycloakProvider>
 )
 ReactDOM.render(<App />, document.getElementById("app"))
